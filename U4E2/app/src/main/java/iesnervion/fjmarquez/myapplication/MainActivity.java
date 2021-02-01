ce2115853e259449d3635b64bd0654a5a47bea62
Francisco José Márquez Pulido <43822565+fjmarquez@users.noreply.github.com>
2021-02-01 22:13:18 +0100

    PPP con SharedPreferences

--- U4E2/app/src/main/java/iesnervion/fjmarquez/myapplication/MainActivity.java
+++ U4E2/app/src/main/java/iesnervion/fjmarquez/myapplication/MainActivity.java
@@ -2,7 +2,12 @@ package iesnervion.fjmarquez.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -24,7 +29,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private ImageView cpuPapel;
     private ImageView cpuTijera;
 
-    private final String[] cpuOpciones = {"piedra", "papel", "tijeras"};
+    private final String[] OPCIONES = {"piedra", "papel", "tijeras"};
+
+    private String jugadaUser = "";
+    private String jugadaCPU = "";
+    private String ganador = "";
+
+    private int userWin = 0;
+    private int cpuWin = 0;
+    private int empate = 0;
+
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editorPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +52,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void first(){
+        preferences = getSharedPreferences("results", Context.MODE_PRIVATE);
+        editorPreferences = preferences.edit();
+
+        userWin = preferences.getInt("usuario", 0);
+        cpuWin = preferences.getInt("cpu", 0);
+        empate = preferences.getInt("empate", 0);
+
         btnPiedra = findViewById(R.id.bntPiedra);
         btnPapel = findViewById(R.id.bntPapel);
         btnTijera = findViewById(R.id.bntTijera);
@@ -62,31 +85,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             case R.id.bntPapel:
                 setVisibility(R.id.userImgPapel);
-                eleccionCPU();
+                electionCPU();
+                electWinner();
                 break;
 
             case R.id.bntPiedra:
                 setVisibility(R.id.userImgPiedra);
-                eleccionCPU();
+                electionCPU();
+                electWinner();
                 break;
 
             case R.id.bntTijera:
                 setVisibility(R.id.userImgTijera);
-                eleccionCPU();
+                electionCPU();
+                electWinner();
                 break;
 
         }
     }
 
-    private void eleccionCPU(){
+    private void electionCPU(){
         Random r = new Random();
         int random = r.nextInt(3 - 0);
 
-        if(cpuOpciones[random] == "piedra"){
+        if(OPCIONES[random] == "piedra"){
             setVisibility(R.id.cpuImgPiedra);
-        }else if(cpuOpciones[random] == "papel"){
+        }else if(OPCIONES[random] == "papel"){
             setVisibility(R.id.cpuImgPapel);
-        }else if(cpuOpciones[random] == "tijeras"){
+        }else if(OPCIONES[random] == "tijeras"){
             setVisibility(R.id.cpuImgTijera);
         }
     }
@@ -97,33 +123,130 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 userPiedra.setVisibility(View.INVISIBLE);
                 userTijera.setVisibility(View.INVISIBLE);
                 userPapel.setVisibility(View.VISIBLE);
+                jugadaUser = OPCIONES[1];
+                Toast.makeText(this, ""+jugadaUser, Toast.LENGTH_SHORT).show();
                 break;
             case R.id.cpuImgPapel:
                 cpuPiedra.setVisibility(View.INVISIBLE);
                 cpuTijera.setVisibility(View.INVISIBLE);
                 cpuPapel.setVisibility(View.VISIBLE);
+                jugadaCPU = OPCIONES[1];
+                Toast.makeText(this, ""+jugadaCPU, Toast.LENGTH_SHORT).show();
                 break;
             case R.id.userImgPiedra:
                 userPiedra.setVisibility(View.VISIBLE);
                 userTijera.setVisibility(View.INVISIBLE);
                 userPapel.setVisibility(View.INVISIBLE);
+                jugadaUser = OPCIONES[0];
+                Toast.makeText(this, ""+jugadaUser, Toast.LENGTH_SHORT).show();
                 break;
             case R.id.cpuImgPiedra:
                 cpuPiedra.setVisibility(View.VISIBLE);
                 cpuTijera.setVisibility(View.INVISIBLE);
                 cpuPapel.setVisibility(View.INVISIBLE);
+                jugadaCPU = OPCIONES[0];
+                Toast.makeText(this, ""+jugadaCPU, Toast.LENGTH_SHORT).show();
                 break;
             case R.id.userImgTijera:
                 userPiedra.setVisibility(View.INVISIBLE);
                 userTijera.setVisibility(View.VISIBLE);
                 userPapel.setVisibility(View.INVISIBLE);
+                jugadaUser = OPCIONES[2];
+                Toast.makeText(this, ""+jugadaUser, Toast.LENGTH_SHORT).show();
                 break;
             case R.id.cpuImgTijera:
                 cpuPiedra.setVisibility(View.INVISIBLE);
                 cpuTijera.setVisibility(View.VISIBLE);
                 cpuPapel.setVisibility(View.INVISIBLE);
+                jugadaCPU = OPCIONES[2];
+                Toast.makeText(this, ""+jugadaCPU, Toast.LENGTH_SHORT).show();
                 break;
         }
+    }
+
+
+    public void electWinner(){
+        if(jugadaUser == OPCIONES[0]){
+            if (jugadaUser == OPCIONES[0] && jugadaCPU == OPCIONES[1]){
+                ganador = "cpu";
+            }else if(jugadaUser == OPCIONES[0] && jugadaCPU == OPCIONES[2]){
+                ganador = "user";
+            }else if(jugadaUser == OPCIONES[0] && jugadaCPU == OPCIONES[0]){
+                ganador = "empate";
+            }
+        }else if (jugadaUser == OPCIONES[1]){
+            if (jugadaUser == OPCIONES[1] && jugadaCPU == OPCIONES[2]){
+                ganador = "cpu";
+            }else if(jugadaUser == OPCIONES[1] && jugadaCPU == OPCIONES[0]){
+                ganador = "user";
+            }else if(jugadaUser == OPCIONES[1] && jugadaCPU == OPCIONES[1]){
+                ganador = "empate";
+            }
+        }else{
+            if (jugadaUser == OPCIONES[2] && jugadaCPU == OPCIONES[0]){
+                ganador = "cpu";
+            }else if(jugadaUser == OPCIONES[2] && jugadaCPU == OPCIONES[1]){
+                ganador = "user";
+            }else if(jugadaUser == OPCIONES[2] && jugadaCPU == OPCIONES[2]){
+                ganador = "empate";
+            }
+        }
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                notifyResult();
+            }
+        }, 1000);
+
+    }
+
+    public void notifyResult(){
+
+        String mensajeGanador;
+
+        if(ganador == "user"){
+            mensajeGanador = "¡Enhorabuena! has ganado";
+
+            userWin = preferences.getInt("user", 0);
+            editorPreferences.putInt("user", userWin+1);
+            editorPreferences.commit();
+            userWin = preferences.getInt("user", 0);
+
+        }else if(ganador == "cpu") {
+            mensajeGanador = "¡Vaya! Has perdido";
+
+            cpuWin = preferences.getInt("cpu", 0);
+            editorPreferences.putInt("cpu", cpuWin+1);
+            editorPreferences.commit();
+            cpuWin = preferences.getInt("cpu", 0);
+
+        }
+        else {
+            mensajeGanador = "¡Ups! Empate";
+
+            empate = preferences.getInt("empate", 0);
+            editorPreferences.putInt("empate", empate+1);
+            editorPreferences.commit();
+            empate = preferences.getInt("empate", 0);
+
+        }
+
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setMessage(mensajeGanador + "\n\n" + "Ganadas por usuario= " + userWin + "\n" + "Ganadas por CPU= " + cpuWin + "\n" + "Empatadas = " + empate);
+        builder1.setTitle("Partida finalizada");
+
+        builder1.setPositiveButton(
+                "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
     }
 
 }
